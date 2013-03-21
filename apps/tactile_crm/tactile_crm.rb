223ecf67051b2c02ec0db3dfa8ff75b882972522
BHOPANNA <gagan@ubuntu.ubuntu-domain>
2013-03-21 15:32:41 +0530

    tactile_crm

---
+++ apps/tactile_crm/tactile_crm.rb
@@ -0,0 +1,110 @@
+module TactileCrm
+  module EventHandler
+    # Handle 'ticket.created' event
+    def ticket_created
+      ticket = payload.ticket
+      requester = ticket.requester
+      person = find_person(requester)
+      if person
+        html = existing_person_info(person)
+      else
+        html = created_person_info(person)
+        person = create_person(requester)
+        update_note(person, ticket)
+        comment_on_ticket(html, ticket)
+      end
+      [200, "Ticket sent"]
+    end
+  end
+end
+
+module TactileCrm
+  module ActionHandler
+    def button
+     # Handle Action here
+     [200, "Success"]
+    end
+  end
+end
+
+module TactileCrm
+  class Base < SupportBeeApp::Base
+    string :api_token, :required => true, :label => 'Tactile Auth Token'
+    string :account_name, :required => true, :label => 'Tactile Account Name'
+    boolean :should_create_person, :default => true, :required => false, :label => 'Create a New Person'
+	
+    white_list :account_name, :should_create_person
+    
+    def find_person(requester)
+      first_name, sur_name = split_name(requester)
+      response = http.get "https://#{settings.account_name}.tactilecrm.com/people" do |req|
+        req.headers['Accept'] = 'application/json'
+        req.params['api_token'] = settings.api_token
+      end
+      people = response.body['people']
+      person = people.select{|pe| pe['firstname'] == 'firstname'}.first
+    end
+
+    def create_person(requester)
+      return unless settings.should_create_person.to_s == '1'
+      first_name, sur_name = split_name(requester)
+      response = http_post "https://#{settings.account_name}.tactilecrm.com/people/save" do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.params['api_token'] = settings.api_token
+        req.body = {Person:{firstname:first_name, surname:first_name}}.to_json
+      end
+      person_id = response.body['id']
+      person = get_person_by_id(person_id)
+    end
+
+    def split_name(requester)
+      first_name, sur_name = requester.name ? requester.name.split : [requester.email,'']
+    end
+
+    def get_person_by_id(person_id)
+      response = http_get "https://#{settings.account_name}.tactilecrm.com/people/view/#{person_id}" do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.params['api_token'] = settings.api_token
+      end
+      person = response.body['person']
+    end
+
+    def update_note(person, ticket)
+      http_post "https://#{settings.account_name}.tactilecrm.com/notes/save" do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params['api_token'] = settings.api_token
+      req.body = {Note:{title:'ticket address', note:generate_note_content(ticket), person_id:person['id']}}.to_json
+      end
+    end
+
+    def comment_on_ticket(html, ticket)
+      ticket.comment(:html => html)
+    end
+ 
+    def existing_person_info(person)
+      html = ""
+      html << "<b> #{person['name']} </b><br/>" 
+      html << "#{person['email']} " if person['email']
+      html << "<br/>"
+      html << person_link(person)
+      html
+    end
+
+    def created_person_info(person)
+      html = "Added <b> #{person['name']} </b> to Tactile... " 
+      html << person_link(person)
+      html
+    end
+    
+    def person_link(person)
+      "<a href='https://#{account_name}.tactilecrm.com/person/view/#{person['id']}'>View #{person['name']}'s profile on Tactile</a>"
+    end
+
+    def generate_note_content(ticket)
+      note = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
+    end
+
+  end
+end
+
+
